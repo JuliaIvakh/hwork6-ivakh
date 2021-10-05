c211f10be8de52a747c2c32c88b7737036fcda99
JuliaIvakh <85705848+JuliaIvakh@users.noreply.github.com>
2021-10-06 00:23:17 +0300

    hw6

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Task 1
+console.log(Boolean(Number("10")) + 1); // Boolean(10)+1= true+1= 1+1= 2
+console.log("sub " + Number(false)); // "sub"+0= sub0
+console.log(16 * "19"); //16*19= 304
+console.log(true - 70); //0-70= -69
+console.log(Number(1 + String(1)) + 1); //Number(11)+1= 11+1= 12
+
+//Task 2
+// let hours = prompt("How many hours?");
+// let SecInHour = alert(60 * 60 * `${hours}`);
+
+//Task 3
+// let num = 1;
+// num += 12;
+// num -= 14;
+// num *= 5;
+// num /= 7;
+// num += 1;
+// num -= 1;
+// alert(num);
